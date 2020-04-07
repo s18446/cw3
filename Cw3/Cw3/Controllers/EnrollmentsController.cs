d9e4cda89f39fff7240ed38231009433b2c2122a
lekhi <danek@poczta.onet.eu>
2020-04-08 01:07:46 +0200

    cw5 zadanie 1 final

--- Cw3/Cw3/Controllers/EnrollmentsController.cs
+++ Cw3/Cw3/Controllers/EnrollmentsController.cs
@@ -32,7 +32,7 @@ namespace Cw3.Controllers
                     // Czy studia istnieja?
                     com.CommandText = "select IdStudy from studies where name=@name";
                     com.Parameters.AddWithValue("name", request.studiesName);
-                    Console.WriteLine("Tutaj jestem1");
+                    //Console.WriteLine("Tutaj jestem1");
                     var dr = com.ExecuteReader();
                     if (!dr.Read())
                     {
@@ -52,31 +52,24 @@ namespace Cw3.Controllers
                     {
                         dr.Close();
                         // dodac nowy enrollment
-                          com.CommandText = "INSERT INTO Enrollment(idEnrollment, Semester, idStudy, StartDate) OUTPUT INSERTED.IDenrollment VALUES( " +
-                                       "(select(select max(idEnrollment) from Enrollment) + 1), 1 , @idStudies,  (SELECT CONVERT(date, getdate())))";
-                             com.Parameters.AddWithValue("idStudies", idStudies);
-                          dr = com.ExecuteReader();
+                        com.CommandText = "INSERT INTO Enrollment(idEnrollment, Semester, idStudy, StartDate) OUTPUT INSERTED.IDenrollment VALUES( " +
+                                     "(select(select max(idEnrollment) from Enrollment) + 1), 1 , @idStudies,  (SELECT CONVERT(date, getdate())))";
+                        com.Parameters.AddWithValue("idStudies", idStudies);
+                        dr = com.ExecuteReader();
                         if (dr.Read())
                         {
                             idEnrollment = (int)dr["IdEnrollment"];
                         }
-                            //   _insertIntoEnrollments(com, idStudies);
-                            dr.Close();
-                        Console.WriteLine("Tutaj jestem3");
-                        //com.CommandText = "select idEnrollment from Enrollment where (StartDate = (select max(StartDate) from Enrollment)) and semester=1 and idStudy = @idStudy2";
-                        //com.Parameters.AddWithValue("idStudy2", idStudies);
-                        //dr = com.ExecuteReader();
-                        //Console.WriteLine("Tutaj jestem4");
-                        // idEnrollment = (int)dr["IdEnrollment"];
-                        //Console.WriteLine("Tutaj jestem5");
-                        //dr.Close();
-                    } else
+                        dr.Close();
+                        //              Console.WriteLine("Tutaj jestem3");
+                    }
+                    else
                     {
                         idEnrollment = (int)dr["IdEnrollment"];
                         dr.Close();
                     }
-                    Console.WriteLine("TUTAJ JESTEM 5");
-                    
+                    //       Console.WriteLine("TUTAJ JESTEM 5");
+
                     Console.WriteLine("Checking doubled indexes.");
                     com.CommandText = "select indexNumber from Student where indexNumber = @index";
                     com.Parameters.AddWithValue("index", request.IndexNumber);
@@ -89,9 +82,9 @@ namespace Cw3.Controllers
                         return BadRequest("Indeks nie jest unikalny.");
                     }
                     dr.Close();
-                 
+
                     //dodawanie studenta do bazy
-                    Console.WriteLine("TUTAJ JESTEM 6");
+                    //     Console.WriteLine("TUTAJ JESTEM 6");
                     com.CommandText = "INSERT INTO Student(IndexNumber, FirstName, LastName, BirthDate, IdEnrollment) VALUES (@indexNumber, @firstName, @lastName, @birthDate, @idEnrollment) ";
                     com.Parameters.AddWithValue("indexNumber", request.IndexNumber);
                     com.Parameters.AddWithValue("firstName", request.FirstName);
@@ -99,17 +92,7 @@ namespace Cw3.Controllers
                     com.Parameters.AddWithValue("birthDate", request.birthDate);
                     com.Parameters.AddWithValue("idEnrollment", idEnrollment);
                     com.ExecuteNonQuery();
-                    Console.WriteLine("TUTAJ JESTEM 7");
-
-                    //tworzenie obiektu enrollment do zwrocenia
-                    //  com.CommandText = "select * from enrollment where idEnrollment = @idEnrollment";
-                    //    com.Parameters.AddWithValue("idEnrollment", idEnrollment);
-                    //    dr = com.ExecuteReader();
-
-                    //   enrollment.idEnrollment = idEnrollment;
-                    //    enrollment.idStudy = (int)dr["IdStudy"];
-                    //   enrollment.semester = (int)dr["Semester"];
-                    //    enrollment.startDate = (DateTime)dr["StartDate"];
+                    //   Console.WriteLine("TUTAJ JESTEM 7");
 
                     response = new EnrollStudentResponse()
                     {
@@ -123,22 +106,12 @@ namespace Cw3.Controllers
                 {
                     Console.WriteLine(ex.Message);
                     tran.Rollback();
-                    return BadRequest("Problem z bazka");       
+                    return BadRequest("Problem z bazka");
                 }
 
                 return Created("Dodano studenta", response);
             }
         }
-
-
-        private void _insertIntoEnrollments(SqlCommand command, int idStudy)
-        {
-            command.CommandText = "insert into Enrollment(IDENROLLMENT, SEMESTER, IDSTUDY, STARTDATE) "
-                                  + "values ((select count(*)+1 from Enrollment), 1, @id_study, getdate())";
-            command.Parameters.AddWithValue("id_study", idStudy);
-            var reader = command.ExecuteReader();
-            command.Parameters.Clear();
-            reader.Close();
         }
 
        
